b264fd65fe279ea9d4c5600c09e3138cf1daf566
SergioGuijarrodeCabo <sguijarrodcabo@gmail.com>
2023-02-09 09:17:43 +0100

    Add project files.

---
+++ ProyectoMentopoker/Models/Celda.cs
@@ -0,0 +1,19 @@
+﻿namespace ProyectoMentopoker.Models
+{
+    public class Celda
+    {
+        public int Table_id { get; set; }       
+        public string Cell_id { get; set; } 
+        public string Background_color { get; set; }    
+        public string Color { get; set; }   
+
+        public Celda(int table_id, string cell_id, string background_color, string color)
+        {
+            this.Table_id = table_id;
+            this.Cell_id = cell_id;
+            this.Background_color = background_color;
+            this.Color = color;
+
+        }
+    }
+}
---
+++ ProyectoMentopoker/Repositories/RepositoryTablas.cs
@@ -0,0 +1,28 @@
+﻿using System.Data.SqlClient;
+namespace ProyectoMentopoker.Repositories
+{
+
+
+    public class RepositoryTablas
+    {
+
+        SqlConnection cn;
+        SqlCommand com;
+        SqlDataReader reader;
+
+        public RepositoryTablas()
+        {
+
+
+            string connectionString = @"Data Source=DESKTOP-E38C8U3\SQLEXPRESS;Initial Catalog=MENTOPOKER;Integrated Security=True";
+            // this.cn = new SqlConnection(HelperConfiguartion.GetConnectionString());
+            this.cn = new SqlConnection(connectionString);
+            this.com = new SqlCommand();
+            this.com.Connection = this.cn;
+        }
+
+
+
+
+    }
+}
