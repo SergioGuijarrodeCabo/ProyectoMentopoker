83224b6cb757e00f0374c981557e9a84a628e2d5
SergioGuijarrodeCabo <sguijarrodcabo@gmail.com>
2023-02-10 09:53:03 +0100

    v6

--- ProyectoMentopoker/Controllers/TablasController.cs
+++ ProyectoMentopoker/Controllers/TablasController.cs
@@ -1,13 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using ProyectoMentopoker.Models;
+using ProyectoMentopoker.Repositories;
 namespace ProyectoMentopoker.Controllers
 {
     public class TablasController : Controller
     {
+        private RepositoryTablas repo;
+
         public IActionResult Index()
         {
+           
             return View();
 
+        }
+
+
+        public IActionResult GetTabla(int id)
+        {
+            this.repo = new RepositoryTablas();
+            List<Celda> tabla = this.repo.GetTabla(id);
+            return View(tabla);
         }
     }
 }
--- ProyectoMentopoker/Repositories/RepositoryTablas.cs
+++ ProyectoMentopoker/Repositories/RepositoryTablas.cs
@@ -1,4 +1,17 @@
 ﻿using System.Data.SqlClient;
+using ProyectoMentopoker.Models;
+
+#region
+//CREATE OR ALTER PROCEDURE SP_GET_TABLA
+//(@ID INT)
+//AS
+//SELECT * FROM Tablas WHERE table_id=@ID
+//GO
+
+
+#endregion
+
+
 namespace ProyectoMentopoker.Repositories
 {
 
@@ -14,7 +27,7 @@ namespace ProyectoMentopoker.Repositories
         {
 
 
-            string connectionString = @"Data Source=DESKTOP-E38C8U3\SQLEXPRESS;Initial Catalog=MENTOPOKER;Integrated Security=True";
+            string connectionString = @"Data Source=LOCALHOST\DESARROLLO;Initial Catalog=PROYECTOMENTOPOKER;User ID=sa;Password=MCSD2022";
             // this.cn = new SqlConnection(HelperConfiguartion.GetConnectionString());
             this.cn = new SqlConnection(connectionString);
             this.com = new SqlCommand();
@@ -22,7 +35,49 @@ namespace ProyectoMentopoker.Repositories
         }
 
 
+        public List<Celda> GetTabla(int id)
+        {
+            List<Celda> tabla = new List<Celda>();
+            string sql = "select * from Tablas where table_id=@ID";
 
+            SqlParameter pamid = new SqlParameter("@ID", id);
+
+            this.com.Parameters.Add(pamid);
+
+            this.com.CommandType = System.Data.CommandType.Text;
+
+            this.com.CommandText = sql;
+
+            this.cn.Open();
+
+            this.reader = this.com.ExecuteReader();
+
+         
+            while (this.reader.Read())
+            {
+                
+                int table_id = int.Parse(this.reader["table_id"].ToString());
+                string cell_id = this.reader["cell_id"].ToString();
+                string background_color = this.reader["background_color"].ToString();
+                string text_color = this.reader["text_color"].ToString();
+
+                Celda celda = new Celda(table_id, cell_id, background_color, text_color);
+                tabla.Add(celda);
+            }
+
+            this.reader.Close();
+
+            this.cn.Close();
+
+            this.com.Parameters.Clear();
+
+            return tabla;
+        
+        }
 
     }
+
+
+
+    
 }
