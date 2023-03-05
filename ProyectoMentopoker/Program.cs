6e5b19017248daa97a3b3bcaa7f017b8a07dee58
SergioGuijarrodeCabo <sguijarrodcabo@gmail.com>
2023-03-05 19:22:07 +0100

    vv

--- ProyectoMentopoker/Program.cs
+++ ProyectoMentopoker/Program.cs
@@ -4,7 +4,7 @@ using ProyectoMentopoker.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 string connectionString =
-    builder.Configuration.GetConnectionString("SqlMentopokerClase");
+    builder.Configuration.GetConnectionString("SqlMentopokerCasa");
 
 
 builder.Services.AddTransient<RepositoryEstadisticas>();
